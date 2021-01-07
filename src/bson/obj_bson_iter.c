6e5a43ea47bf084af20883f8dd898ebf99d2764f
wcy <2296176046@qq.com>
2021-01-07 00:18:47 -0800

    bson-iter-2

--- src/bson/obj_bson_iter.c
+++ src/bson/obj_bson_iter.c
@@ -353,7 +353,68 @@ const obj_bson_value_t *obj_bson_iter_value(obj_bson_iter_t *iter) {
 
 /* visitor */
 obj_bool_t obj_bson_iter_visit_all(obj_bson_iter_t *iter, const obj_bson_visitor_t *visitor, void *data) {
+    obj_bson_type_t bson_type;
+    const char *key;
+    /* loop until  */
+    while (obj_bson_iter_next_internal(iter, &key, &bson_type)) {
+        /* validate key */
+        /* if (*key ) */
+        switch (bson_type) {
+            /* can't reach here */
+            case OBJ_BSON_TYPE_EOO:
+                break;
+            case OBJ_BSON_TYPE_DOUBLE:
+                if (visitor->visit_double && visitor->visit_double(iter, key, obj_bson_iter_double(iter), data)) {
+                    return true;
+                }
+                break;
+            case OBJ_BSON_TYPE_UTF8: {
+                const char *utf8;
+                obj_int32_t utf8_len;
+                utf8 = obj_bson_iter_utf8(iter, &utf8_len);
+                /* TODO validate utf8 string */
+                if (visitor->visit_utf8 && visitor->visit_utf8(iter, key, utf8_len, utf8, data)) {
+                    return true;
+                }
+                break;
+            }
+            case OBJ_BSON_TYPE_OBJECT:
 
+            case OBJ_BSON_TYPE_ARRAY:
+
+            case OBJ_BSON_TYPE_BINARY: {
+                const obj_uint8_t *binary;
+                obj_int32_t binary_len;
+                binary = obj_bson_iter_binary(iter, &binary_len);
+                if (visitor->visit_binary && visitor->visit_binary(iter, key, binary_len, binary, data)) {
+                    return true;
+                }
+                break;
+            }
+            case OBJ_BSON_TYPE_BOOL:
+                if (visitor->visit_bool && visitor->visit_bool(iter, key, obj_bson_iter_bool(iter), data)) {
+                    return true;
+                }
+                break;
+            case OBJ_BSON_TYPE_NULL:
+                if (visitor->visit_null && visitor->visit_null(iter, key, data)) {
+                    return true;
+                }
+                break;
+            case OBJ_BSON_TYPE_INT32:
+                if (visitor->visit_int32 && visitor->visit_int32(iter, key, obj_bson_iter_int32(iter), data)) {
+                    return true;
+                }
+                break;
+            case OBJ_BSON_TYPE_INT64:
+                if (visitor->visit_int64 && visitor->visit_int64(iter, key, obj_bson_iter_int64(iter), data)) {
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 
